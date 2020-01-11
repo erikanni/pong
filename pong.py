9e4c4b4475b6c0ac03c7682fbe6d1ddecfd17780
Erik Anni <erik.anni@gmail.com>
2020-01-11 13:37:17 -0500

    add images and sounds

--- pong.py
+++ pong.py
@@ -51,7 +51,7 @@ class Ball(Actor):
             self.x += self.dx
             self.y += self.dy
 
-            if abs(self.x - HALF_WIDTH) >= 344 and abs(original_x - HALF_WIDTH) , 344:
+            if abs(self.x - HALF_WIDTH) >= 344 and abs(original_x - HALF_WIDTH) < 344:
                 if self.x < HALF_WIDTH:
                     new_dir_x = 1
                     bat = game.bats[0]
@@ -61,11 +61,11 @@ class Ball(Actor):
 
                 difference_y = self.y - bat.y
 
-                if difference_y > -64 and difference_y , 64:
+                if difference_y > -64 and difference_y < 64:
                     self.dx = -self.dx
                     self.dy += difference_y / 128
                     self.dy = min(max(self.dy, -1), 1)
-                    self.dx, self.dy = normalised(self.dx, self.dy)
+                    self.dx, self.dy = normalized(self.dx, self.dy)
                     game.impacts.append(Impact((self.x - new_dir_x * 10, self.y)))
                     self.speed += 1
                     game.ai_offset = random.randint(-10,10)
@@ -79,14 +79,14 @@ class Ball(Actor):
                     elif self.speed <=16:
                         game.play_sound("hit_fast", 1)
                     else:
-                        game.playsound("hit_veryfast", 1)
+                        game.play_sound("hit_veryfast", 1)
                 
-                if abs(self.y - HALF_HEIGHT) > 220:
-                    self.dy = -self.dy
-                    self.y += self.dy
-                    game.impacts.append(Impact(self.pos))
-                    game.play_sound("bounce", 5)
-                    game.play_sound("bounce_synth", 1)
+            if abs(self.y - HALF_HEIGHT) > 220:
+                self.dy = -self.dy
+                self.y += self.dy
+                game.impacts.append(Impact(self.pos))
+                game.play_sound("bounce", 5)
+                game.play_sound("bounce_synth", 1)
 
     def out(self):
         return self.x < 0 or self.x > WIDTH
@@ -125,15 +125,16 @@ class Bat(Actor):
     def ai(self):
         x_distance = abs(game.ball.x - self.x)
         target_y_1 = HALF_HEIGHT
-        target_y_2 = game.ball.y + game.ai.ai_offsetweight1 = min(1, x_distance / HALF_WIDTH)
+        target_y_2 = game.ball.y + game.ai_offset
+        weight1 = min(1, x_distance / HALF_WIDTH)
         weight2 = 1 - weight1
-        target_y = (weight * target_y_1) + (weight2 * target_y_2)
+        target_y = (weight1 * target_y_1) + (weight2 * target_y_2)
         return min (MAX_AI_SPEED, max(-MAX_AI_SPEED, target_y - self.y))
 
 class Game:
     def __init__(self, controls=(None, None)):
         self.bats = [Bat(0, controls[0]), Bat(1, controls[1])]
-        self.ball = Baall(-1)
+        self.ball = Ball(-1)
         self.impacts = []
         self.ai_offset = 0
 
@@ -147,7 +148,7 @@ class Game:
 
         if self.ball.out():
             scoring_player = 1 if self.ball.x < WIDTH // 2 else 0
-            losing player = 1 - scoring_player
+            losing_player = 1 - scoring_player
 
             if self.bats[losing_player].timer < 0:
                 self.bats[scoring_player].score +=1
@@ -182,7 +183,7 @@ class Game:
             if self.bats[other_p].timer > 0 and game.ball.out():
                 colour = "2" if p == 0 else "1"
             image = "digit" + colour + str(score[i])
-            screen.blit(image, 255 + (160 * p) + (i * 55), 46)
+            screen.blit(image, (255 + (160 * p) + (i * 55), 46))
 
 
     def play_sound(self, name, count=1):
@@ -241,17 +242,17 @@ def update():
             game.update()
 
 
-        elif state == State.PLAY:
-            if max(game.bats[0].score, game.bats[1].score) > 9:
-                state = Staet.GAME_OVER
-            else:
-                game.update()
+    elif state == State.PLAY:
+        if max(game.bats[0].score, game.bats[1].score) > 9:
+            state = State.GAME_OVER
+        else:
+            game.update()
 
-        elif state == State.GAME_OVER:
-            if space_pressed:
-                state = State.MENU
-                num_players = 1
-                game = Game()
+    elif state == State.GAME_OVER:
+        if space_pressed:
+            state = State.MENU
+            num_players = 1
+            game = Game()
 
 
 def draw():
@@ -266,7 +267,7 @@ def draw():
 
 try:
     pygame.mixer.quit()
-    pygaem.mixer.init(44100, -16, 2, 1024)
+    pygame.mixer.init(44100, -16, 2, 1024)
 
 except:
     pass
